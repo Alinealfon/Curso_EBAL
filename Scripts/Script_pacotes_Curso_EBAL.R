6185c2a7b8c2eaa0fc483d9da097806982a12771
Aline Fonseca <alineafonseca@gmail.com>
2026-06-29 20:48:08 -0300

    Add files via upload

---
+++ Scripts/Script_pacotes_Curso_EBAL.R
@@ -0,0 +1,42 @@
+## Pacotes Necessários para rodar as lições do Curso EBAL ##
+
+# Instalar os Pacotes
+#Obs. Este passo deve ser realizado somente uma vez.
+
+install.packages("dplyr")
+install.packages("nortest")
+install.packages("sjPlot")
+install.packages("lattice")
+install.packages("effects")
+install.packages("lme4")
+install.packages("lmerTest")
+install.packages("arm")
+install.packages("ordinal")
+install.packages("carData")
+install.packages("Matrix")
+install.packages("MASS")
+install.packages("png")
+install.packages("som")
+
+# Sempre que abrir o RStudio para começar um trabalho, defina seu diretório de trabalho!
+## Para o curso EBAL, o diretório precisa ser a pasta que contém os arquivos de dataframes.
+
+setwd()
+
+## Abrir Pacotes
+## Obs. Este passo deve ser realizado todas as vezes que o curso for acessado.
+
+library(dplyr)
+library(sjPlot)
+library(nortest)
+library(effects)
+library(lattice)
+library(lme4)
+library(lmerTest)
+library(arm)
+library(ordinal)
+library(carData)
+library(MASS)
+library(Matrix)
+library(png)
+library(som)
